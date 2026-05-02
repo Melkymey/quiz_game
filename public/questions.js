e0fe66cc84cf15699535c577a762c07c4700d77f
meikelop <142100454+Melkymey@users.noreply.github.com>
2026-05-03 02:53:41 +0900

    Add questions data to questions.js

---
+++ public/questions.js
@@ -0,0 +1,12 @@
+window.questions = [
+  {
+    question: "Ibukota Indonesia?",
+    correct: "Jakarta",
+    answers: ["Jakarta","Bandung","Surabaya","Medan"]
+  },
+  {
+    question: "2 + 2 = ?",
+    correct: "4",
+    answers: ["3","4","5","6"]
+  }
+];
